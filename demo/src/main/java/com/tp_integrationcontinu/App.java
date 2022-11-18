7e082ebdbe6558213306713d67df96a89ce9d7e3
Ulrichhap <50375568+Ulrichhap@users.noreply.github.com>
2022-11-18 16:08:32 +0100

    Add files via upload

---
+++ demo/src/main/java/com/tp_integrationcontinu/App.java
@@ -0,0 +1,9 @@
+package com.tp_integrationcontinu;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "CICD rocks!" );
+    }
+}
---
+++ demo/src/test/java/com/tp_integrationcontinu/AppTest.java
@@ -0,0 +1,20 @@
+package com.tp_integrationcontinu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
